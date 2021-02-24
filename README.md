d06957b304c5411cbe41de5fb8a6c4ec5b42a8cb
Marshall <magj2006@gmail.com>
2021-02-24 22:27:11 +0500

    Allocating

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Implementing Vec from scratch
 
 1. Layout  
     Unique 标准库的Unique没有对它的内容做任何有趣的保证，因此不需要new, 因此避免传入null
+
+2. 关于内存分配文档中写的通过heap, 但是官方已经标记为不稳定， 所以改用alloc，希望后面能顺利
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,68 @@
-#![feature(ptr_internals)]
+#![feature(alloc_internals, ptr_internals)]
 
-use std::ptr::{self, Unique};
+use std::alloc::rust_oom;
+use std::alloc::{alloc, realloc, Layout};
+use std::mem;
+use std::ptr::Unique;
 
-pub struct Vec<T> {
+pub struct MyVec<T> {
     ptr: Unique<T>,
     cap: usize,
     len: usize,
+}
+
+impl<T> MyVec<T> {
+    pub fn new() -> Self {
+        assert!(mem::size_of::<T>() != 0, "we're not ready to hanld ZSTs");
+        MyVec {
+            ptr: Unique::dangling(),
+            cap: 0,
+            len: 0,
+        }
+    }
+
+    fn grow(&mut self) {
+        // this is all pretty delicate, so let's say it's all unsafe
+        unsafe {
+            // current API requires us to specify size and alignment manually.
+            let align = mem::align_of::<T>();
+            let elem_size = mem::size_of::<T>();
+
+            let layout = Layout::from_size_align_unchecked(elem_size, align);
+
+            let (new_cap, ptr) = if self.cap == 0 {
+                let ptr = alloc(layout);
+                (1, ptr)
+            } else {
+                // as an invariant, we can assume that `self.cap < isize::MAX`,
+                // so this doesn't need to be checked.
+                let new_cap = self.cap * 2;
+                // Similarly this can't overflow due to previously allocating this
+                let old_num_bytes = self.cap * elem_size;
+
+                // check that the new allocation doesn't exceed `isize::MAX` at all
+                // regardless of the actual size of the capacity. This combines the
+                // `new_cap <= isize::MAX` and `new_num_bytes <= usize::MAX` checks
+                // we need to make. We lose the ability to allocate e.g. 2/3rds of
+                // the address space with a single Vec of i16's on 32-bit though.
+                // Alas, poor Yorick -- I knew him, Horatio.
+                assert!(
+                    old_num_bytes <= (isize::MAX as usize) / 2,
+                    "capacity overflow"
+                );
+
+                let new_num_bytes = old_num_bytes * 2;
+                let ptr = realloc(self.ptr.as_ptr() as *mut _, layout, new_num_bytes);
+                (new_cap, ptr)
+            };
+
+            // If allocate or reallocate fail, we'll get `null` back
+            if ptr.is_null() {
+                rust_oom(layout);
+            }
+
+            self.ptr = Unique::new(ptr as *mut _).unwrap();
+            self.cap = new_cap;
+        }
+    }
 }
